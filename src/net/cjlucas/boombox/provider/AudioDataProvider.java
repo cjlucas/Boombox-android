67b522661f0c399954ccc2b9e18f03a4782562c4
Chris Lucas <chris@chrisjlucas.com>
2014-02-20 14:26:08 -0700

    [provider] no longer a subclass of Thread

--- src/net/cjlucas/boombox/provider/AudioDataProvider.java
+++ src/net/cjlucas/boombox/provider/AudioDataProvider.java
@@ -1,11 +1,11 @@
 package net.cjlucas.boombox.provider;
 
-abstract class AudioDataProvider extends Thread
+public abstract class AudioDataProvider
 {
-    public static final int STATUS_EOF_REACHED = -1;
-    public static final int STATUS_ERROR_OCCURED = -2;
+	public static final int STATUS_EOF_REACHED   = -1;
+	public static final int STATUS_ERROR_OCCURED = -2;
 
-    abstract boolean prepare();
-    abstract int onNeedData(byte[] buffer);
-    abstract void release();
+	abstract boolean prepare();
+	abstract int onNeedData(byte[] buffer);
+	abstract void release();
 }
