693af8a8ada3d11e00d2d3c2d76e647181089fe9
Chris Lucas <chris@chrisjlucas.com>
2014-02-20 20:44:58 -0700

    [Boombox] implemented most of the stubs

--- src/net/cjlucas/boombox/Boombox.java
+++ src/net/cjlucas/boombox/Boombox.java
@@ -3,9 +3,8 @@ package net.cjlucas.boombox;
 import android.media.MediaPlayer;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Queue;
 
 import net.cjlucas.boombox.provider.AudioDataProvider;
 
@@ -20,60 +19,88 @@ MediaPlayer.OnSeekCompleteListener
 {
 	private List<AudioDataProvider> providers;
 	private List<AudioDataProvider> playlist;
-	private int queueCursor;
+	private int playlistCursor;
 	private List<MediaPlayer> players;
+	private boolean shuffleMode;
+	private boolean continuousMode;
 
 	public Boombox()
 	{
-		this.providers = new ArrayList<AudioDataProvider>();
-		this.playlist  = new ArrayList<AudioDataProvider>();
-		this.players   = new ArrayList<MediaPlayer>();
+		this.providers      = Collections.synchronizedList( new ArrayList<AudioDataProvider>() );
+		this.playlist       = Collections.synchronizedList( new ArrayList<AudioDataProvider>() );
+		this.players        = Collections.synchronizedList( new ArrayList<MediaPlayer>() );
+		this.playlistCursor = 0;
+		this.shuffleMode    = false;
+		this.continuousMode = false;
 	}
 
 	// Providers Management
 
+	private int getNextPlaylistCursor()
+	{
+		int newCursor = this.playlistCursor + 1;
+
+		return this.continuousMode ? newCursor % this.playlist.size() : -1;
+	}
+
+	private int getPreviousPlaylistCursor()
+	{
+		int newCursor = this.playlistCursor - 1;
+
+		if (newCursor < 0) {
+			return this.continuousMode ? this.playlist.size() - 1 : -1;
+		} else {
+			return newCursor;
+		}
+	}
+
 	public boolean hasNextProvider()
 	{
-		// TODO
-		return false;
+		return getNextPlaylistCursor() != -1;
 	}
 
 	public boolean hasPreviousProvider()
 	{
-		// TODO
-		return false;
+		return getPreviousPlaylistCursor() != -1;
 	}
 
 	public void addProvider(AudioDataProvider provider)
 	{
-		// TODO
+		this.providers.add(provider);
+		this.playlist.add(provider);
 	}
 
 	public AudioDataProvider getCurrentProvider()
 	{
-		// TODO
-		return null;
+		return this.playlist.get(playlistCursor);
 	}
 
 	public AudioDataProvider getNextProvider()
 	{
-		// TODO
-		return null;
+		int nextCursor = getNextPlaylistCursor();
+
+		return nextCursor == -1 ? null : this.playlist.get(nextCursor);
 	}
 
 	public AudioDataProvider getPreviousProvider()
 	{
-		// TODO
-		return null;
+		int prevCursor = getPreviousPlaylistCursor();
+
+		return prevCursor == -1 ? null : this.playlist.get(prevCursor);
 	}
 
 	// Playback Controls
+
+	private boolean hasCurrentPlayer()
+	{
+		return this.players.size() > 0;
+	}
 
 	private MediaPlayer getCurrentPlayer()
 	{
 		MediaPlayer player = null;
 
-		if (this.players.size() > 0) {
+		if ( hasCurrentPlayer() ) {
 			player = this.players.get(0);
 		}
 
@@ -82,27 +109,72 @@ MediaPlayer.OnSeekCompleteListener
 
 	public void play()
 	{
-		// TODO
+		MediaPlayer mp = getCurrentPlayer();
+
+		if (mp != null) {
+			mp.start();
+		}
 	}
 
 	public void pause()
 	{
-		// TODO
+		MediaPlayer mp = getCurrentPlayer();
+
+		if (mp != null) {
+			mp.pause();
+		}
 	}
 
 	public void togglePlayPause()
 	{
-		// TODO
+		MediaPlayer mp = getCurrentPlayer();
+
+		if ( mp == null || !mp.isPlaying() ) {
+			play();
+		} else {
+			pause();
+		}
 	}
 
 	public void setShuffleMode(boolean shuffle)
 	{
-		// TODO
+		boolean oldMode = isShuffleModeEnabled();
+		this.shuffleMode = shuffle;
+
+		// prevent reshuffling if already shuffled
+		if (oldMode == false) {
+			// do some shuffling
+		}
 	}
 
 	public void setContinuousMode(boolean continuous)
 	{
-		// TODO
+		this.continuousMode = continuous;
+	}
+
+	public boolean isShuffleModeEnabled()
+	{
+		return this.shuffleMode;
+	}
+
+	public boolean isContinuousModeEnabled()
+	{
+		return this.continuousMode;
+	}
+
+	// Helpers
+
+	private MediaPlayer createMediaPlayer()
+	{
+		MediaPlayer player = new MediaPlayer();
+		player.setOnBufferingUpdateListener(this);
+		player.setOnCompletionListener(this);
+		player.setOnErrorListener(this);
+		player.setOnInfoListener(this);
+		player.setOnPreparedListener(this);
+		player.setOnSeekCompleteListener(this);
+
+		return player;
 	}
 
 	// MediaPlayer Callbacks
